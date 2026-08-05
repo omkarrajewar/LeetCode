a9acd44aaf463fe414e9983b3001ea42c8d9c1c0
Omkar Rajewar <rajewaromkar@gmail.com>
2026-08-05 14:42:58 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (92.79%) - LeetSync

--- 1927-maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.java
+++ 1927-maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.java
@@ -2,22 +2,18 @@ class Solution {
 
     public int maxAscendingSum(int[] nums) {
 
-        int maxSum = 0;
+        int currentSum = nums[0];
+        int maxSum = nums[0];
 
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 1; i < nums.length; i++) {
 
-            int sum = nums[i];
-            maxSum = Math.max(maxSum, sum);
-
-            for (int j = i + 1; j < nums.length; j++) {
-
-                if (nums[j] > nums[j - 1]) {
-                    sum += nums[j];
-                    maxSum = Math.max(maxSum, sum);
-                } else {
-                    break;
-                }
+            if (nums[i] > nums[i - 1]) {
+                currentSum += nums[i];
+            } else {
+                currentSum = nums[i];
             }
+
+            maxSum = Math.max(maxSum, currentSum);
         }
 
         return maxSum;
