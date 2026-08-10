9c4a176c458f5e23cfee79a008ee25919319e388
Omkar Rajewar <rajewaromkar@gmail.com>
2026-08-10 14:39:53 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (61.35%) - LeetSync

---
+++ 413-arithmetic-slices/arithmetic-slices.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numberOfArithmeticSlices(int[] nums) {
+       int n=nums.length;
+       if(n<3){
+        return 0;
+       }
+       int current=0;
+       int total=0;
+
+       for(int i=2;i<n;i++){
+        if(nums[i]- nums[i-1]==nums[i-1]- nums[i-2]){
+            current++;
+            total=total+current;
+
+        }
+        else{
+            current=0;
+
+        }
+       }
+       return total;
+    }
+}
