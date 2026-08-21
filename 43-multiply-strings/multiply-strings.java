66c64f46fb62c1df8fb63bdb8c99f1da6e38e183
Omkar Rajewar <rajewaromkar@gmail.com>
2026-08-21 23:10:06 +0530

    Time: 3 ms (78.87%) | Memory: 43.8 MB (33.14%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.java
@@ -0,0 +1,50 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+
+        // If either number is 0
+        if (num1.equals("0") || num2.equals("0")) {
+            return "0";
+        }
+
+        int n = num1.length();
+        int m = num2.length();
+
+        // Maximum possible length is n + m
+        int[] result = new int[n + m];
+
+        // Multiply each digit
+        for (int i = n - 1; i >= 0; i--) {
+
+            for (int j = m - 1; j >= 0; j--) {
+
+                int digit1 = num1.charAt(i) - '0';
+                int digit2 = num2.charAt(j) - '0';
+
+                int product = digit1 * digit2;
+
+                int pos1 = i + j;
+                int pos2 = i + j + 1;
+
+                int sum = product + result[pos2];
+
+                result[pos2] = sum % 10;
+                result[pos1] += sum / 10;
+            }
+        }
+
+        // Convert result array to String
+        StringBuilder sb = new StringBuilder();
+
+        for (int digit : result) {
+
+            // Skip leading zeros
+            if (sb.length() == 0 && digit == 0) {
+                continue;
+            }
+
+            sb.append(digit);
+        }
+
+        return sb.toString();
+    }
+}
