c307ceafbe1206b24fc728d10ca68f4b994544d3
Omkar Rajewar <rajewaromkar@gmail.com>
2026-09-08 14:10:01 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (99.67%) - LeetSync

---
+++ 1970-sorting-the-sentence/sorting-the-sentence.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String sortSentence(String s) {
+        String[] words=s.split(" ");
+        String[] result=new String[words.length];
+        for(String word:words){
+            int position=word.charAt(word.length()-1)-'0';
+
+            String originalWord=word.substring(0,word.length()-1);
+            result[position-1]= originalWord;
+        }
+
+        return String.join(" ",result);
+    }
+}
